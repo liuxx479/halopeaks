d84f813e49ff1fc8deca2f1b5ed65431e2923f3b
liuxx479 <liuxx479@gmail.com>
2015-05-21 13:11:13 -0400

    update amiga2snapshot.py

--- amiga2snapshot.py
+++ amiga2snapshot.py
@@ -9,41 +9,34 @@ import numpy as np
 from scipy import *
 import sys, glob, os
 sys.modules["mpi4py"] = None
-#from scipy.integrate import quad
-#import scipy.optimize as op
-#from scipy import interpolate
-#import matplotlib.pyplot as plt
-#from pylab import *
-#import matplotlib.gridspec as gridspec
-#from scipy import ndimage as snd
 from lenstools.simulations import Gadget2Snapshot
+from astropy.units import Mpc,m,s
+from emcee.utils import MPIPool
 
 home = '/work/02977/jialiu/lenstools_home/'
 storage = '/scratch/02977/jialiu/lenstools_storage/'
+ID_arr = genfromtxt(os.path.join(home, 'realizations.txt'), dtype=str)
+#ID = 'Om0.300_Ol0.700|512b240|ic1'
 
-#snapshots_dir = '/scratch/02977/jialiu/lenstools_storage/Om0.300_Ol0.700/512b240/ic1/snapshots/'
-#amiga_dir = '/scratch/02977/jialiu/lenstools_storage/Om0.300_Ol0.700/512b240/ic1/amiga/'
-#snap_amiga_dir = '/scratch/02977/jialiu/lenstools_storage/Om0.300_Ol0.700/512b240/ic1/snapshots_amiga/'
-ID = 'Om0.300_Ol0.700|512b240|ic1'
-######### reads in .AHF_particles ###########
-
-def halo_particles(ID, snap_id):
+def halo_particles(IDsnap_id):
 	'''
 	input: 
 	ID = e.g. 'Om0.300_Ol0.700|512b240|ic1'
 	snap_id = N, where N runs from 0 to total number of snapshots
 	i = n, where n is one of the 16 split files for each snapshots
 	'''
+	ID, snap_id = IDsnap_id
 	cosmo_id,geometry_id, ic_id = ID.split("|")
 	
 	#### file names for gadget snap and AHF particles
-	#isnap_fn = lambda i: os.path.join(storage, cosmo_id, geometry_id, ic_id, 'snapshots/snapshot_%03d.%i'%(snap_id, i))#####
-	#ihalo_fn = lambda i: os.path.join(storage, cosmo_id, geometry_id, ic_id, 'amiga/snap%i.%04d.z0.000.AHF_particles'%(snap_id, i))
-	halo_fn_arr = glob.glob(os.path.join(storage, cosmo_id, geometry_id, ic_id, 'amiga'))
+	amiga_dir = os.path.join(storage, cosmo_id, geometry_id, ic_id, 'amiga')
+	os.system('mkdir -p %s'%(amiga_dir))
+	halo_fn_arr = glob.glob(amiga_dir+'/*particle*')
 	snap_fn_arr = glob.glob(os.path.join(storage, cosmo_id, geometry_id, ic_id, 'snapshots/snapshot_%03d.*'%(snap_id)))
 	
-	halo_fn_arr = glob.glob("*particles")
-	snap_fn_arr = glob.glob('snapshot_060.*')
+	##### test on laptop #####
+	#halo_fn_arr = glob.glob("*particles")
+	#snap_fn_arr = glob.glob('snapshot_060.*')
 	
 	txt_amiga = concatenate([genfromtxt(halo_fn, skiprows=2) for halo_fn in halo_fn_arr], axis = 0).T
 	ID_amiga = txt_amiga[0][txt_amiga[1]==1]
@@ -60,4 +53,17 @@ def halo_particles(ID, snap_id):
 	halo_ID_position = map(ihalo_ID_position, snap_fn_arr)
 	halo_ID = concatenate([halo_ID_position[i][0] for i in range(len(halo_ID_position))])
 	halo_position = concatenate([halo_ID_position[i][1] for i in range(len(halo_ID_position))], axis=0)
-	return halo_ID, halo_position
+	
+	halo_snap = Gadget2Snapshot()
+	hg = Gadget2Snapshot.open(snap_fn_arr[0]).header #header_gadget
+	halo_snap.setHeaderInfo(Om0=hg['Om0'], Ode0=hg['Ode0'], w0=hg['w0'], wa=hg['wa'], h=hg['h'], redshift=hg['redshift'], box_size=hg['box_size'])
+	halo_snap.setPositions(array(halo_position)*Mpc)
+	
+	halo_snap.write(os.path.join(storage, cosmo_id, geometry_id, ic_id, 'snapshots_amiga/snapshot_%03d.*'%(snap_id)), files = len(snap_fn_arr))
+
+	### test on laptop
+	#halo_snap.write('snapshots_amiga/snapshot_%03d'%(snap_id), files = len(snap_fn_arr))
+
+pool = MPIPool()
+pool.map(halo_particles, [[ID, snap_id] for ID in ID_arr, snap_id in range(61)])
+
